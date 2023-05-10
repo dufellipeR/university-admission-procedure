c6cb4698d870548ad4e4d479924c41f57d2cdd3d
dufelliper <eduardo.fellipe30@gmail.com>
2023-05-10 08:17:32 -0300

    walk in folder

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
 func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Println("Directory is not specified")
+	} else {
+		err := filepath.Walk(os.Args[1], func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if !info.IsDir() {
+				fmt.Println(path)
+			}
+
+			return nil
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 
 }
