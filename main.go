96a1c87c94361a15ab43bdabc5c64ee532adcb94
dufelliper <eduardo.fellipe30@gmail.com>
2023-05-13 22:44:04 -0300

    finalizing...

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 	"crypto/md5"
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"path/filepath"
 	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -20,37 +20,126 @@ func handleInput() string {
 
 func selectOption() string {
 	var sortOption string
-	for {
-		fmt.Println("Size sorting options:")
-		fmt.Println("1. Descending")
-		fmt.Println("2. Ascending")
 
-		sortOption = handleInput()
-		if sortOption == "1" || sortOption == "2" {
-			return sortOption
-		}
+	fmt.Println("\nSize sorting options:")
+	fmt.Println("1. Descending")
+	fmt.Println("2. Ascending")
 
-		fmt.Println("Wrong option")
+	fmt.Println("\nEnter a sorting option:")
+	sortOption = handleInput()
+
+	switch sortOption {
+	case "1":
+		return sortOption
+	case "2":
+		return sortOption
+	default:
+		fmt.Println("\nWrong option")
+		selectOption()
+
 	}
-
+	return ""
 }
 
 func hashFile(path string) []byte {
 	file, err := os.Open(path)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
-	defer file.Close()
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(file)
 
 	md5Hash := md5.New()
 	// Copy the data from 'hello.txt' to the 'md5Hash' interface until reaching EOF:
 	if _, err := io.Copy(md5Hash, file); err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
 	return md5Hash.Sum(nil)
 }
 
-func checkDuplicates(files map[int64][]string, sortedKeys []int64) {
+func validateFiles(filesToValidate []string, files []string) bool {
+
+	isValid := false
+	for _, val := range filesToValidate {
+		i, err := strconv.Atoi(val)
+		if err != nil {
+			return false
+		}
+		for k := range files {
+			if i == k+1 {
+				isValid = true
+				break
+			}
+		}
+	}
+	return isValid
+}
+
+func selectFiles(files []string) []string {
+	fmt.Println("\nEnter file numbers to delete:")
+	fileNumbers := handleInput()
+	splitFileNumbers := strings.Split(fileNumbers, " ")
+
+	if len(splitFileNumbers) == 0 {
+		fmt.Println("\nWrong format")
+		return selectFiles(files)
+
+	} else if len(splitFileNumbers) > len(files) {
+		fmt.Println("\nWrong format")
+		return selectFiles(files)
+
+	} else {
+		isValid := validateFiles(splitFileNumbers, files)
+
+		if !isValid {
+			fmt.Println("\nWrong format")
+			return selectFiles(files)
+		}
+	}
+	return splitFileNumbers
+}
+
+func deleteFiles(files []string) int64 {
+
+	fmt.Println("\nDelete files?")
+	input := handleInput()
+
+	switch input {
+	case "yes":
+		var freedUpSize int64
+		selectedFiles := selectFiles(files)
+
+		for _, validFile := range selectedFiles {
+			getIndex, _ := strconv.Atoi(validFile)
+
+			fileInfo, err := os.Stat(files[getIndex-1])
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			freedUpSize += fileInfo.Size()
+
+			err = os.Remove(files[getIndex-1])
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+
+		return freedUpSize
+	case "no":
+		return 0
+	default:
+		fmt.Println("\nWrong option")
+		deleteFiles(files)
+	}
+	return 0
+}
+
+func checkDuplicates(files map[int64][]string, sortedKeys []int64) []string {
 	fmt.Println("Check for duplicates?")
 	input := handleInput()
 
@@ -60,18 +149,23 @@ func checkDuplicates(files map[int64][]string, sortedKeys []int64) {
 		var controller []string
 
 		for _, value := range sortedKeys {
+			categorized := false
 			duplicated := make(map[string][]string)
 
 			if len(files[value]) > 1 {
-				fmt.Println("")
-				fmt.Println(value, " bytes")
 
 				for _, filePath := range files[value] {
 					hashPath := fmt.Sprintf("%x\n", hashFile(filePath))
 					duplicated[hashPath] = append(duplicated[hashPath], filePath)
 				}
 				for key, hashValue := range duplicated {
+
 					if len(hashValue) > 1 {
+						if !categorized {
+							fmt.Println("")
+							fmt.Println(value, " bytes")
+							categorized = true
+						}
 						fmt.Printf("Hash: %s", key)
 						for index := range hashValue {
 							fmt.Printf("%d. %s \n", counter, hashValue[index])
@@ -84,12 +178,17 @@ func checkDuplicates(files map[int64][]string, sortedKeys []int64) {
 
 			}
 		}
+
+		return controller
+
 	case "no":
 		break
 	default:
 		fmt.Println("Wrong option")
 		checkDuplicates(files, sortedKeys)
 	}
+
+	return []string{}
 
 }
 
@@ -99,18 +198,21 @@ func main() {
 
 	var keys []int64
 
+	var freedSpace int64
+
 	if len(os.Args) != 2 {
 		fmt.Println("Directory is not specified")
 
 	} else {
-		fmt.Println("Enter file format:")
+		fmt.Println("\nEnter file format:")
 		fileFormat := handleInput()
 
 		sorter := selectOption()
+		fmt.Print("\n")
 
 		err := filepath.Walk(os.Args[1], func(path string, info os.FileInfo, err error) error {
 			if err != nil {
-				log.Fatal(err)
+				fmt.Println(err)
 			}
 
 			if !info.IsDir() {
@@ -125,10 +227,10 @@ func main() {
 			return nil
 		})
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println(err)
 		}
 
-		for key, _ := range files {
+		for key := range files {
 			keys = append(keys, key)
 		}
 
@@ -145,10 +247,18 @@ func main() {
 			for _, value := range files[keys[index]] {
 				fmt.Println(value)
 			}
-			fmt.Println("")
+			fmt.Print("\n")
 		}
 
-		checkDuplicates(files, keys)
+		controller := checkDuplicates(files, keys)
+
+		if len(controller) != 0 {
+			freedSpace = deleteFiles(controller)
+			if freedSpace != 0 {
+				fmt.Printf("\nTotal freed up space: %d bytes", freedSpace)
+			}
+
+		}
 
 	}
 
